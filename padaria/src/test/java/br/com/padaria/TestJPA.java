3b84e44fd9c44dc175eaa60e240a67a045b227ce
Jaíne Rosa <jaine.rosa_11@live.com>
2024-05-14 22:04:04 -0300

    :construction: feat(main):Configuracao do Hibernate

---
+++ padaria/src/test/java/br/com/padaria/TestJPA.java
@@ -0,0 +1,12 @@
+package br.com.padaria;
+
+import javax.persistence.Persistence;
+
+public class TestJPA {
+
+	public static void main(String[] args) {
+		
+		Persistence.createEntityManagerFactory("padaria");
+		
+	}
+}
